461a0642530313eb9f6ad02c07cb6d625cd24fd0
szapp <dev.szapp@gmail.com>
2013-12-23 17:13:33 +0100

    Cleans up ajax call to distributor

--- af/ajaxDistributor.php
+++ af/ajaxDistributor.php
@@ -10,22 +10,16 @@
 $rp = '../';
 require_once($rp . 'af/global.php');
 
+$path = findDistributor()->pathTo();
 $clientname= explode(".", strtolower(gethostbyaddr($_SERVER['REMOTE_ADDR'])));
 
 // Check whether protocol is installed
 $query = 'SELECT `client` FROM `protocol_' . $protocol . '` WHERE `client`="' . $clientname[0] . '"';
 if (!$result = $db->query($query))
-	die("Could not retrieve protocol check from database");
-$protocol_inst = false;
-if ($finfo = $result->fetch_array())
-	$protocol_inst = true;
+	exit(json_encode(array('error' => 'Could not retrieve protocol check from database')));
+if ($finfo = $result->fetch_array()) // If protocol was installed return path of most suitable database with exe
+	echo json_encode(array('error' => 0, 'installed' => true, 'protocol' => $protocol . '://', 'path' => $path));
+else
+	echo json_encode(array('error' => 0, 'installed' => false, 'exec' => $protocol_exec, 'path' => $path));
 $result->close();
-
-// If protocol was installed
-if ($protocol_inst) {
-	// Return path of most suitable database with exe
-	echo json_encode(array('installed' => true, 'protocol' => $protocol . '://', 'path' => findDistributor()->pathTo()));
-} else {
-	echo json_encode(array('installed' => false, 'exec' => $protocol_exec, 'path' => findDistributor()->pathTo()));
-}
 ?>
